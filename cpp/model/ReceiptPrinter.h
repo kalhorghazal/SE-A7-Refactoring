bcf46332de63bd6241745c8f5fed8ca229b21084
Ghazal Kalhor <55555804+kalhorghazal@users.noreply.github.com>
2021-01-12 17:24:07 +0330

    Separate Declarations and Definitions

--- cpp/model/ReceiptPrinter.h
+++ cpp/model/ReceiptPrinter.h
@@ -11,81 +11,28 @@
 #define NEW_LINE "\n"
 #define SPACE " "
 
-std::string operator*(std::string const &s, std::size_t n)
-{
-  std::string r;
-  r.reserve(n * s.size());
-  for (std::size_t i = 0; i < n; ++i)
-    r += s;
-  return r;
-}
-
 class ReceiptPrinter
 {
 
 public:
-    ReceiptPrinter() : ReceiptPrinter(COLUMN_NUM)
-    {
-    }
+    ReceiptPrinter() : ReceiptPrinter(COLUMN_NUM) {}
 
-    ReceiptPrinter(int columns) : columns(columns)
-    {}
+    ReceiptPrinter(int columns) : columns(columns) {}
 
-    std::string printReceipt(const Receipt &receipt)
-    {
-        std::string result;
-        for (const auto &item : receipt.getItems())
-        {
-            result.append(presentReceiptItem(item));
-        }
-        for (const auto &discount : receipt.getDiscounts())
-        {
-            result.append(presentDiscount(discount));
-        }
-        result.append(NEW_LINE);
-        result.append(presentTotal(receipt));
-        return result;
-    }
+    std::string printReceipt(const Receipt &receipt);
 
-    std::string presentReceiptItem(const ReceiptItem &item) const
-    {
-        std::string price = getFormattedNumberAsString(item.getTotalPrice(), 2);
-        std::string name = item.getProductName();
+    std::string presentReceiptItem(const ReceiptItem &item) const;
 
-        std::string line = formatLineWithWhitespace(name, price);
+    std::string presentDiscount(const Discount &discount) const;
 
-        if (item.getQuantity() != 1)
-        {
-            line += (std::string(SPACE) * 2) + getFormattedNumberAsString(item.getPrice(), 2) + " * " + presentQuantity(item) + NEW_LINE;
-        }
-        return line;
-    }
+    std::string presentTotal(const Receipt &receipt) const;
 
-    std::string presentDiscount(const Discount &discount) const
-    {
-        std::string name = discount.getDescription() + "(" + discount.getProductName() + ")";
-        std::string pricePresentation = getFormattedNumberAsString(discount.getDiscountAmount(), 2);
-        return formatLineWithWhitespace(name, pricePresentation);
-    }
+    std::string formatLineWithWhitespace(const std::string &name, const std::string &value) const;
 
-    std::string presentTotal(const Receipt &receipt) const
-    {
-        std::string total = "Total: ";
-        std::string pricePresentation = presentPrice(receipt.getTotalPrice());
-        return formatLineWithWhitespace(total, pricePresentation);
-    }
-
-    std::string formatLineWithWhitespace(const std::string &name, const std::string &value) const
-    {
-        int whitespaceSize = columns - name.length() - value.length();
-        std::string formatted = name + (std::string(SPACE) * whitespaceSize) + value + NEW_LINE;
-        return formatted;
-    }
-    std::string presentPrice(double price) const
-    { return getFormattedNumberAsString(price, 2); }
+    std::string presentPrice(double price) const;
 
 
-    static std::string presentQuantity(const ReceiptItem &item)
+    static std::string presentQuantity(const ReceiptItem &item) 
     {
         if (item.hasEachProductUnit()) {
             return getFormattedNumberAsString(item.getQuantity(), 0);
